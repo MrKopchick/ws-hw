7245e7239934cc7a02525ef57702a3b02e6328fd
illakoroliuk <illakoroluk62@gmail.com>
2025-06-16 23:17:18 +0300

    rename build folder

--- dist/javascript/routes/routes.js
+++
@@ -1,7 +0,0 @@
-import { router as gameRouter } from "./game.js";
-import { router as signinRouter } from "./signin.js";
-const rootRouter = (app) => {
-    app.use("/signin", signinRouter);
-    app.use("/game", gameRouter);
-};
-export { rootRouter };
--- dist/javascript/routes/signin.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import path from "node:path";
-import { HTML_FILES_PATH } from "../config.js";
-const router = Router();
-router.get("/", (_request, response) => {
-    const page = path.join(HTML_FILES_PATH, "signin.html");
-    response.sendFile(page);
-});
-export { router };
